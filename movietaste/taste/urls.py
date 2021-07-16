0e6e53741aca186e590ed1cb2507787b8c26de3b
jinsuc28 <86936634+jinsuc28@users.noreply.github.com>
2021-07-16 10:47:47 +0900

    base start

---
+++ movietaste/taste/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+app_name = 'taste'
+
+urlpatterns = [
+    path('', movie_in_category, name='movie_all'),
+    path('<slug:category_slug>/', movie_in_category, name='movie_in_category'),
+    path('<int:id>/<movie_slug>/', movie_detail, name='movie_detail'),
+]
