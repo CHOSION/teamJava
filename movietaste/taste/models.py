09485dfca4cd569b1f555b67bf9c116ec12eba42
jinsuc28 <86936634+jinsuc28@users.noreply.github.com>
2021-07-22 11:52:46 +0900

    model list

--- movietaste/taste/models.py
+++ movietaste/taste/models.py
@@ -15,16 +15,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('taste:product_in_category', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('taste:movie_in_category', args=[self.slug])
 
 class Movie(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='movies')
+    category_name = models.CharField(max_length=100, null=True)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, allow_unicode=True)
     image = models.ImageField(upload_to='movies/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
-    meta_description = models.TextField(blank=True)
+    review = models.TextField(blank=True)
+    director = models.TextField(blank=True)
+    rating = models.TextField(blank=True)
+    open_date = models.TextField(blank=True)
+    youtube = models.URLField(blank=True)
     available_display = models.BooleanField('Display', default=True)
 
     created = models.DateTimeField(auto_now_add=True)
